5d7f7d9bcc7b94ca238f61054c2d47156ffe82bd
liama482 <liamabbate@hanovernorwichschools.org>
2016-09-23 13:34:47 -0400

    Updated from Brython Server: 9/23/2016 1:34:47 PM

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 """
 fizzbuzz.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Liam
+Credit: 
 
 Assignment:
 
@@ -44,3 +44,7 @@ Fizz
 Fizz
 Buzz
 """
+
+b = input("How many numbers shall we print? ")
+fizz = input ("For multiples of what number shall we print 'Fizz'? ")
+buzz = input("For multiples of what number shall we print 'Buzz'? ")
